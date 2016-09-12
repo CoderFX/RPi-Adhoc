8501f6c8ae7d63844869180aba093606af836dc9
Mindaugas <gelumind@gmail.com>
2016-09-12 15:38:42 +0300

    Update setup-adhoc.sh

--- setup-adhoc.sh
+++ setup-adhoc.sh
@@ -45,30 +45,105 @@ sudo cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bck
 #Clean /etc/dhcp/dhcpd.conf
 sudo truncate -s 0 /etc/dhcp/dhcpd.conf
 
+#Guide: http://lcdev.dk/2012/11/18/raspberry-pi-tutorial-connect-to-wifi-or-create-an-encrypted-dhcp-enabled-ad-hoc-network-as-fallback/
 #Edit /etc/dhcp/dhcpd.conf
 sudo echo "
+DHCPDARGS=wlan0; #args for the dhcpd daemon -> limit DHCP to the wlan0 interface
 ddns-update-style interim;
+
 default-lease-time 600;
 max-lease-time 7200;
+
 authoritative;
 log-facility local7;
+
+option subnet-mask 255.255.255.0;
+option broadcast-address 192.168.1.255;
+option domain-name "RPi-network";
+option routers 192.168.1.1; #default gateway
+
 subnet 192.168.1.0 netmask 255.255.255.0 {
- range 192.168.1.5 192.168.1.150;
+    range 192.168.1.2 192.168.1.20; #IP range to offer
+}
+
+#static IP-assignment
+host Phone {
+    hardware ethernet 11:aa:22:bb:33:cc;
+    192.168.1.100;
+}
+
+host PC {
+    hardware ethernet 11:aa:22:bb:33:cd;
+    192.168.1.101;
 }
 " >> /etc/dhcp/dhcpd.conf
 
+#where N should be replaced by your target line number ====================> Find correct line number
+sed -i 'Ns/.*/INTERFACES="wlan0"/' /etc/default/isc-dhcp-server
+
+#Create backup
+sudo cp /etc/rc.local /etc/rc.local.bck
+#Clean original /etc/rc.local
+sudo truncate -s 0 /etc/rc.local
+
+# RPi Network Conf Bootstrapper ====================> Edit with full script
+sudo echo "
+createAdHocNetwork(){
+    echo "Creating ad-hoc network"
+    ifconfig wlan0 down
+    iwconfig wlan0 mode ad-hoc
+    iwconfig wlan0 key aaaaa11111 #WEP key
+    iwconfig wlan0 essid PiAdHoc      #SSID
+    ifconfig wlan0 192.168.1.200 netmask 255.255.255.0 up
+    /usr/sbin/dhcpd wlan0
+    echo "Ad-hoc network created"
+}
+ 
+echo "================================="
+echo "RPi Network Conf Bootstrapper 0.1"
+echo "================================="
+echo "Scanning for known WiFi networks"
+ssids=( 'MyWlan' 'MyOtherWlan' )        # =================== Can be edited ===================
+connected=false
+for ssid in "${ssids[@]}"
+do
+    if iwlist wlan0 scan | grep $ssid > /dev/null
+    then
+        echo "First WiFi in range has SSID:" $ssid
+        echo "Starting supplicant for WPA/WPA2"
+        wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null 2>&1
+        echo "Obtaining IP from DHCP"
+        if dhclient -1 wlan0
+        then
+            echo "Connected to WiFi"
+            connected=true
+            break
+        else
+            echo "DHCP server did not respond with an IP lease (DHCPOFFER)"
+            wpa_cli terminate
+            break
+        fi
+    else
+        echo "Not in range, WiFi with SSID:" $ssid
+    fi
+done
+ 
+if ! $connected; then
+    createAdHocNetwork
+fi
+ 
+exit 0
+" >> /etc/rc.local
+
+
+
+
+
+
+
+
+
+
+
 #Reboot to make everything working
 sudo reboot
-
-
-
-
-
-
-
-
-
-
-
-
-
